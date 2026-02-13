b3fd6480f29fb1876ec9fa36ae77e1dc8c07a9da
Tilakr21 <tilakrepalle21@gmail.com>
2026-02-13 11:29:21 +0530

    action

--- vm_creation.sh
+++ vm_creation.sh
@@ -1,14 +1,15 @@
 for instance in $@; do
+    HOSTED_ZONE_ID=Z048988723HS7J4W6I2HO
         if [ $instance == nginx ]; then
             ip=$(aws ec2 run-instances \
                     --image-id ami-0220d79f3f480ecf5 \
                     --instance-type t3.small \
                     --security-group-ids sg-0039b9fd218e26beb \
                     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'
-                    --associate-public-ip-address \
                     --query "Instances[0].PublicIpAddress" \
                     --output text
                 )
+            echo "Instance Public IP: $ip"
             aws route53 change-resource-record-sets \
                 --hosted-zone-id $HOSTED_ZONE_ID \
                 --change-batch '{
@@ -35,7 +36,6 @@ for instance in $@; do
                   --instance-type t3.small \
                   --security-group-ids sg-0039b9fd218e26beb \
                   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'
-                  --associate-public-ip-address \
                   --query "Instances[0].PrivateIpAddress" \
                   --output text
                 )
