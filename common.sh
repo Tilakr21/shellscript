27275ea201e47e7c3532abee89d81f20d7d5a738
Tilakr21 <tilakrepalle21@gmail.com>
2026-02-13 13:14:04 +0530

    action

--- common.sh
+++ common.sh
@@ -10,9 +10,9 @@ mkdir -p $LOG_DIR
 
 check_user(){
     if [ $user -eq 0 ]; then
-       echo -e "Executing the script through root user...."
+       echo -e "Executing the script through root user...." | tee -a $LOG_FILE
     else
-      echo -e "Need the root access for executing the script..."
+      echo -e "Need the root access for executing the script..." | tee -a $LOG_FILE
       exit 1
     fi
 }
@@ -20,9 +20,9 @@ check_user(){
 
 validation(){
     if [ $1 -eq 0 ]; then
-      echo -e "$(date "+%Y-%m-%d %H:%M:%S") $2 .... $G sucess"
+      echo -e "$(date "+%Y-%m-%d %H:%M:%S") $2 .... $G sucess" | tee -a $LOG_FILE
     else 
-      echo -e "$(date "+%Y-%m-%d %H:%M:%S") $2 .... $R Failure"
+      echo -e "$(date "+%Y-%m-%d %H:%M:%S") $2 .... $R Failure" | tee -a $LOG_FILE
       exit 1
     fi
 }
--- mongodb.sh
+++ mongodb.sh
@@ -5,5 +5,10 @@ check_user
 cp mongo.repo /etc/yum.repos.d/
 validation $? "Copying of mongo.repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y  &>> $LOG_FILE
 validation $? "Installing mongodb"
+
+systemctl enable mongod  &>> LOG_FILE
+validation $? "Enable the mongod service ...."
+systemctl start mongod 
+validation $? "Starting the mongod Service ..."
