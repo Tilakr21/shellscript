34482c6cc98f9d221e565805b939bd02fe88575a
Tilakr21 <tilakrepalle21@gmail.com>
2026-02-13 17:48:28 +0530

    action

--- catalogue.sh
+++ catalogue.sh
@@ -13,7 +13,7 @@ service
 cp $script_dir/mongo.repo /etc/yum.repos.d/
 validation $? "Copying the mongo repo was ..."
 
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y &>> $LOG_FILE
 validation $? "Installing the mongodb client was ..."
 
 INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
@@ -21,7 +21,7 @@ INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().
 if [ $INDEX -le 0 ]; then
    cd /app
    mongosh --host  $MONGODB_HOST < /app/db/master-data.js &>> $LOG_FILE
-   VALIDATE $? "Loading products was ..."
+   validation $? "Loading products was ..."
 else 
   echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Products already loaded ... $Y SKIPPING $N" &>> $LOG_FILE
 fi
