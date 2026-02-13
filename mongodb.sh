a5485e8fc5e983e5d7a2f980d4423320b4915667
Tilakr21 <tilakrepalle21@gmail.com>
2026-02-13 12:12:09 +0530

    action

--- mongodb.sh
+++ mongodb.sh
@@ -5,14 +5,14 @@ Y="\e[33m"
 N="\e[0m"
 
 validation(){
-    if [$1 -eq 0]; then
+    if [ $1 -eq 0 ]; then
       echo -e "date +%Y-%M-%D $2 .... $G sucess"
     else 
       echo -e "date +%Y-%M-%D $2 .... $R Failure"
     fi
 }
 
-if [$user -eq 0]; then
+if [ $user -eq 0 ]; then
    cp mongo.repo /etc/yum.repos.d/
    validation $? "Copying of mongo.repo"
    dnf install mongodb-org -y 
