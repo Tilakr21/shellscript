7854637cc0bbfbede84abe4aa000f9a62ee97ec7
Tilakr21 <tilakrepalle21@gmail.com>
2026-02-14 13:10:26 +0530

    action

--- redis.sh
+++ redis.sh
@@ -8,4 +8,5 @@ script_dir=$PWD
 check_user
 app_setup
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+validation $? "Edit the /etc/redis.conf file ..."
 service 
