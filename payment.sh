2b629ef2fdd62d77d1efbc080fb61050108b6efe
Tilakr21 <tilakrepalle21@gmail.com>
2026-02-14 13:59:46 +0530

    action

--- payment.sh
+++ payment.sh
@@ -6,6 +6,7 @@ runtime=python
 check_user
 
 dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+validation $? "python installation was..."
 
 user_creation
 application
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ./common.sh
 app_name=rabbitmq
+runtime=rabbitmq-server
 script_dir=$PWD
 
 check_user
@@ -8,9 +9,19 @@ check_user
 cp $script_dir/$app_name.repo /etc/systemd/system/
 validation $? "Copying repo ...."
 
-rabbitmq-server
 app_setup
-service
+
+systemctl enable $runtime &>>$LOG_FILE
+validation $? "Enable the $app_name service ...."
+
+systemctl daemon-reload
+validation $? "Reloading the daemon ."
+
+systemctl start $runtime 
+validation $? "Starting the $runtime Service ."
+
+systemctl restart $runtime
+validation $? "Restarting the $runtime Service ..."
 
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
